4a3ad47e14d7420e6823dcb6943c7888617b9490
sunyu <1017565253@qq.com>
2018-12-20 17:20:23 +0800

    一级分类-添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -2,7 +2,7 @@ $(function () {
     var currentPage = 1; //当前页
     var pageSize = 5; //当前条数
 
-    //一进入页面,发送ajax请求,完成渲染
+    //1.一进入页面,发送ajax请求,完成渲染
     render();
 
     function render() {
@@ -40,5 +40,66 @@ $(function () {
             }
         })
     }
+
+    //2.给添加按钮注册点击事件 显示模态框
+    $('#addBtn').click(function () {
+        $('#addModal').modal('show');
+
+    })
+
+    //3.调用表单校验插件,完成校验
+    $('#form').bootstrapValidator({
+        //配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        //检验字段
+        fields: {
+            //校验用户名，对应input表单的name属性
+            categoryName: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        //提示信息
+                        message: '请输入一级分类名称'
+                    },
+
+                }
+            },
+        }
+    })
+
+    //4.阻止默认的提交,通过ajax提交
+    $('#form').on('success.form.bv', function (e) {
+        e.preventDefault();
+        //阻止默认提交 
+
+        //发送ajax提交
+        $.ajax({
+            type: 'post',
+            url: '/category/addTopCategory',
+            data: $('#form').serialize(), //表单序列化
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+
+                if (info.success) {
+                    //关闭模态框
+                    $('#addModal').modal('hide');
+                    //重新渲染第一页
+                    currentPage = 1;
+                    render();
+
+                    //重置表单内容
+                    //resetForm(true) 表示内容和状态都重置
+                    $("#form").data('bootstrapValidator').resetForm(true); //获取表单校验实例
+                }
+            }
+        })
+
+    })
 
 })
